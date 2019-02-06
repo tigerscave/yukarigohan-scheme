bff75a91781767eeab2df19476bbb4e2562b0432
Yukari Ishibashi <ishibashi.yukari511@gmail.com>
2019-02-07 02:47:29 +0900

    write function 'tup+*'

---
+++ tupplusstar.scm
@@ -0,0 +1,16 @@
+(define atom?
+ (lambda (x)
+  (and (not (pair? x))(not (null? x)))))
+
+
+
+(define tup+*
+ (lambda (tup)
+  (cond
+   ((null? tup) 0)
+   ((atom? (car tup))(+ (car tup)(tup+* (cdr tup))))
+     (else (tup+* (car tup))))))
+
+
+(print (tup+* '(1 (2 (3)))))
+(print (tup+* '(1 (2 (3 4) 5))))
