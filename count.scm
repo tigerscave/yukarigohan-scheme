8792d219843f4aa738e598c423ca2a59605aa3ff
Yukari Ishibashi <ishibashi.yukari511@gmail.com>
2019-02-08 17:33:44 +0900

    Write count function

---
+++ count.scm
@@ -0,0 +1,9 @@
+(define count
+ (lambda (a lat)
+  (cond
+   ((null? lat) 0)
+   ((eq? a (car lat))(+ 1 (count a (cdr lat))))
+    (else (count a (cdr lat))))))
+
+(print (count 'a '(a b a v d a)))
+(print (count 'apple '(apple banana orange apple)))
