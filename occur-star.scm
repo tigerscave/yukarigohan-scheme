6aa94e6f83476d8175527d0da3550dfae6e7a494
yukarigohan <ishibashi.yukari511@gmail.com>
2019-02-16 14:02:38 +0900

    Write occur-star function

--- occur-star.scm
+++ occur-star.scm
@@ -1,3 +1,19 @@
 (define atom?
  (lambda (x)
   (and (not (pair? x))(not (null? x)))))
+
+(define add1
+ (lambda (n)
+  (+ n 1)))
+
+(define occur*
+ (lambda (a l)
+  (cond
+   ((null? l) 0)
+   ((atom? (car l))
+    (cond
+     ((eq? a (car l))(add1 (occur* a (cdr l))))
+      (else (occur* a (cdr l)))))
+    (else (+ (occur* a (car l))(occur* a (cdr l)))))))
+
+(print (occur* 'banana '((banana)(split ((((banana ice)))(cream (banana)) sherbet))(banana)(bread)(banana brandy))))
